b89df6715ff18977c08254f4f2ef0ffdda288e5b
ccoroiu <coroiu.cosmin.90@gmail.com>
2020-11-10 23:58:12 +0200

    Initial Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private LayerMask platformsLayerMask;
+    private Transform playerTransform;
+    private Rigidbody2D rigidbody2d;
+    private BoxCollider2D boxCollider2d;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+    private float lastPositionY;
+
+    public float jumpVelocity;
+    public float fallMultiplier;
+    public float lowJumpMultiplier;
+    public float moveSpeed;
+    public bool facingRight;
+    
+
+    void Awake()
+    {
+        playerTransform = transform.GetComponent<Transform>();
+        rigidbody2d = transform.GetComponent<Rigidbody2D>();
+        boxCollider2d = transform.GetComponent<BoxCollider2D>();
+        spriteRenderer = transform.GetComponent<SpriteRenderer>();
+        animator = transform.GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Application.targetFrameRate = 60; // to be removed from here
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovement();
+
+        lastPositionY = playerTransform.position.y;
+    }
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D raycastHit2d = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down, .1f, platformsLayerMask);
+
+        bool isGrounded = raycastHit2d.collider != null;
+        animator.SetBool("OnGround", isGrounded);
+        return isGrounded;
+    }
+
+    private void FlipPlayer(float horizontal)
+    {
+        if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
+        {
+            if (IsGrounded())
+            {
+                animator.SetTrigger("Turn");
+            }
+            facingRight = !facingRight;
+            Vector3 scale = transform.localScale;
+            scale.x *= -1;
+            transform.localScale = scale;
+        }
+    }
+
+    private void HandleMovement()
+    {
+        // JUMP
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetBool("JumpStart", true);
+            rigidbody2d.velocity = Vector2.up * jumpVelocity;
+        }
+        if (rigidbody2d.velocity.y < 0)
+        {
+            rigidbody2d.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rigidbody2d.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            rigidbody2d.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+        if (!IsGrounded() && lastPositionY > playerTransform.position.y)
+        {
+            animator.SetBool("JumpFall", true);
+            animator.SetBool("JumpStart", false);
+        }
+        
+        // LEFT
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            animator.SetFloat("MoveSpeed", Mathf.Abs(moveSpeed));
+
+            FlipPlayer(-moveSpeed);
+
+            rigidbody2d.velocity = new Vector2(-moveSpeed, rigidbody2d.velocity.y);
+        }
+
+        // RIGHT
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            animator.SetFloat("MoveSpeed", Mathf.Abs(moveSpeed));
+
+            FlipPlayer(+moveSpeed);
+
+            rigidbody2d.velocity = new Vector2(+moveSpeed, rigidbody2d.velocity.y);
+        }
+
+        // No keys pressed
+        else if (IsGrounded())
+        {
+            animator.SetFloat("MoveSpeed", 0);
+            animator.SetBool("JumpFall", false);
+
+            rigidbody2d.velocity = new Vector2(0, rigidbody2d.velocity.y);
+        }
+    }
+}
